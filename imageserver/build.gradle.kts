19339d00efe9ed3999a99654f67a7cc62fcf9794
keddad <keddad@yandex.ru>
2021-12-02 17:49:27 +0300

    Add support for .webp images

--- imageserver/build.gradle.kts
+++ imageserver/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-serialization:$ktor_version")
+    implementation(files("$projectDir/src/main/resources/webp-imageio-core-0.1.3.jar"))
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
